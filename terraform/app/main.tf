7900731fb05cbbc2fecf7dd0b27b151c479f30fc
arhat82 <37156937+arhat82@users.noreply.github.com>
2022-08-04 22:54:54 -0300

    Update main.tf

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -1,1 +1,38 @@
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = var.region
+  network       = google_compute_network.custom-test.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
 
+resource "google_compute_network" "custom-test" {
+  name                    = "test-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_service_account" "default" {
+  account_id   = "white-byway-349416"
+  display_name = "Service Account"
+}
+
+resource "google_compute_instance" "instance-apigee-network" {
+  name         = "instance-apigee-netwthork"
+  machine_type = "e2-medium"
+  zone         = "europe-west2-a"
+
+  tags = ["tuvieja", "entanga"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.network-with-private-secondary-ip-ranges.id
+  }
+}
