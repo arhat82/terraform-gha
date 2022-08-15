035e9375594f459df7db3ccf500518cdf87409a4
arhat82 <37156937+arhat82@users.noreply.github.com>
2022-08-15 00:22:35 -0300

    Update main.tf

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -38,3 +38,46 @@ resource "google_compute_instance" "instance-apigee-network" {
     subnetwork = google_compute_subnetwork.network-with-private-secondary-ip-ranges.id
   }
 }
+
+#------------------------------------------------------------------------------------------------
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
