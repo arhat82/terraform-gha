5bbf0f7a10c52c16efd72c09b2636eb4080b86a2
arhat82 <37156937+arhat82@users.noreply.github.com>
2022-09-03 20:04:03 -0300

    Update main.tf

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -15,30 +15,3 @@ resource "aws_subnet" "my_subnet" {
     Name = "tf-example"
   }
 }
-
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-
-resource "aws_instance" "foo" {
-  ami           = "ami-090fa75af13c156b4" # us-west-2
-  instance_type = "t2.micro"
-  
-  tags = {
-    Name = "HelloWorldAgain"
-  }
-
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
-}
